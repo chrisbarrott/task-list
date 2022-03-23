756612c00809e7125b1958d55fe77b568b080d3b
chrisbarrott <chris.barrott@gmail.com>
2022-03-23 16:35:48 +0000

    moved server api

--- api/server-gin.go
+++ api/server-gin.go
@@ -4,6 +4,63 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var Tasks []Task
+
+func RunServer() {
+	r := gin.Default()
+
+	taskRoutes := r.Group("/tasks")
+
+	{
+		taskRoutes.GET("/", ListTasks)
+		taskRoutes.POST("/", NewTask)
+		//taskRoutes.PUT("/:id", UpdateTask)
+		//taskRoutes.PUT("/:id",CompleteTask)
+	}
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve "localhost:8080"
+}
+
+func ListTasks(c *gin.Context) {
+	// null
+	c.JSON(200, Tasks)
+}
+
+func NewTask(c *gin.Context) {
+	var reqBody Task
+
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(422, gin.H{
+			"error":   true,
+			"message": "Invalid request body",
+		})
+	}
+
+	Tasks = append(Tasks, reqBody)
+
+	c.JSON(200, gin.H{
+		"error": false,
+	})
+}
+
+//func getHandler(c *gin.Context) {
+//	c.JSON(200, Tasks) // Return nil
+//}
+
+//func UpdateTask() {
+
+//}
+
+//func CompleteTask() {
+
+//}
+
+/*
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -13,3 +70,4 @@ func main() {
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+*/
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/task-list/api"
 	"github.com/task-list/cmd"
 	"github.com/task-list/data"
 )
-
-var Tasks []api.Task
 
 func main() {
 	data.OpenDatabase()
@@ -15,54 +11,5 @@ func main() {
 	//srv := api.NewServer()
 	//http.ListenAndServe(":8080", srv)
 	//api.StartServer()
-	//runServer()
-}
-
-func runServer() {
-	r := gin.Default()
-
-	taskRoutes := r.Group("/tasks")
-
-	{
-		taskRoutes.GET("/", ListTasks)
-		taskRoutes.POST("/", NewTask)
-		//		taskRoutes.PUT("/:id", UpdateTask)
-	}
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve "localhost:8080"
-}
-
-//func getHandler(c *gin.Context) {
-//	c.JSON(200, Tasks) // Return nil
-//}
-
-func ListTasks(c *gin.Context) {
-	// null
-	c.JSON(200, Tasks)
-}
-
-//func UpdateTask() {
-
-//}
-
-func NewTask(c *gin.Context) {
-	var reqBody api.Task
-
-	if err := c.ShouldBindJSON(&reqBody); err != nil {
-		c.JSON(422, gin.H{
-			"error":   true,
-			"message": "Invalid request body",
-		})
-	}
-
-	Tasks = append(Tasks, reqBody)
-
-	c.JSON(200, gin.H{
-		"error": false,
-	})
+	//api.RunServer()
 }
